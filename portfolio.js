04a4312c4281be17df02ede51173f1dab09acfa2
melwinks05 <melwinks75@gmail.com>
2025-06-09 11:38:35 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,6 @@
 const toggleBtn = document.getElementById('theme-toggle');
 toggleBtn.addEventListener('click', () => {
   const html = document.documentElement;
-  html.dataset.theme = html.dataset.theme === 'dark' ? 'light' : 'dark';
+  const currentTheme = html.getAttribute('data-theme');
+  html.setAttribute('data-theme', currentTheme === 'dark' ? 'light' : 'dark');
 });
